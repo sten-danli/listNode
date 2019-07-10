4e940455b320c3cc7e9415336d7eda052e6f9104
Papa ist Blod <akiraryo@hotmail.com>
2019-07-10 23:40:21 +0200

    push_back加入

--- listNode/listNode/listNode.cpp
+++ listNode/listNode/listNode.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<assert.h>
 using namespace std;
 
 class Objects
@@ -8,11 +9,13 @@ public:
 	{}
 	virtual ~Objects()
 	{}
-private:
-	int data;
+public:
+	virtual void Print()const = 0;//纯虚函数的另外名字叫接口
 };
+class List;
 class ListNode
 {
+	friend List;
 public:
 	ListNode()
 	{
@@ -22,6 +25,7 @@ public:
 	ListNode(Objects* pobj)
 	{
 		data = pobj;
+		next = NULL;
 	}
 	~ListNode()
 	{
@@ -40,10 +44,15 @@ public:
 	{
 		Head = Tail = new ListNode;
 	}
-	~List()
+	void Push_Back(Objects* p)
 	{
-
+		ListNode* s = new ListNode(p);
+		assert(s != NULL);
+		Tail->next = s;
+		Tail = s;
+		
 	}
+	~List(){}
 private:
 	ListNode* Head;
 	ListNode* Tail;
@@ -54,6 +63,11 @@ class IntObjects : public Objects
 public:
 	IntObjects(int d = 0): data(d)
 	{}
+public:
+	void Print()const//IntObjects继承了Object父类就必须实现他的纯虚函数。
+	{
+
+	}
 	~IntObjects()
 	{
 	}
@@ -68,5 +82,6 @@ int main()
 	for (int i = 1; i <= 5; ++i)
 	{
 		IntObjects* pi = new IntObjects(i);
+		mylist.Push_Back(pi);
 	}
 }
